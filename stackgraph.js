60ae700210ca5f8a7a9eca916692d95e9f0e2926
sirahd <sirah@uw.edu>
2017-05-02 17:24:06 -0700

    .

--- stackgraph.js
+++ stackgraph.js
@@ -3,50 +3,44 @@ var margin = {
   top: 20,
   right: 40,
   bottom: 30,
-  left: 30
+  left: 100
 };
+
+var datearray = [];
+var strokecolor = "#000000";
 
 var width = 1000 - margin.left - margin.right;
 var height = 600 - margin.top - margin.bottom;
 var barHeight = 100;
 
-var tooltip = d3.select("body")
-  .append("div")
-  .attr("class", "remove")
-  .style("position", "absolute")
-  .style("z-index", "20")
-  .style("visibility", "hidden")
-  .style("top", "30px")
-  .style("left", "55px");
-
-var x = d3.scaleTime()
+var x = d3.scaleUtc()
   .range([0, width]);
 
 var y = d3.scaleLinear()
   .range([height - 10, 0]);
 
-var z = d3.scaleLinear()
-  .domain([0, 22])
-  .range([0, 1]);
+var z = d3.scaleOrdinal(d3.schemeCategory20);
 
 var xAxis = d3.axisBottom(x)
-  .ticks(d3.timeYear.every(10));
+  .ticks(7);
 
-var yAxisLeft = d3.axisLeft(y);
-var yAxisRight = d3.axisRight(y);
+var yAxisLeft = d3.axisLeft(y)
+  .tickFormat(d3.format(".0%"));
+var yAxisRight = d3.axisRight(y)
+  .tickFormat(d3.format(".0%"));
 
 var stack = d3.stack()
   .offset(d3.stackOffsetExpand)
-  .keys(category.sort())
+  .keys(category.sort().reverse())
 
 var svg = d3.select(".chart").append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
+  .attr("position", "absolute")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("out.csv", (error, data) => {
-  console.log(d3.extent(data, function(d) { return +d.salary; }))
   var nest = d3.nest()
     .key((d) => d.year);
 
@@ -65,7 +59,6 @@ d3.csv("out.csv", (error, data) => {
   var layers = stack(result);
 
   x.domain(d3.extent(data, (d) => d.year));
-
   var min = d3.min(layers, function(d) {
     return d3.min(d, function(d) {
       return d3.min(d);
@@ -79,7 +72,6 @@ d3.csv("out.csv", (error, data) => {
   y.domain([min, max]);
 
   var area = d3.area()
-    .curve(d3.curveCardinal)
     .x(function(d) {
       return x(d.data.key);
     })
@@ -94,9 +86,9 @@ d3.csv("out.csv", (error, data) => {
     .attr("d", function(d) {
       return area(d);
     })
-    .style("fill", (d, i) => d3.interpolateRainbow(Math.random()))
+    .style("fill", (d, i) => z(i))
     .on("click", function(d) {
-      draw_histogram(d.key, d[0].data.values);
+      // draw_histogram(d.key, d[0].data.values);
     });
 
 
@@ -120,118 +112,118 @@ d3.csv("out.csv", (error, data) => {
       svg.selectAll(".layer").transition()
         .duration(250)
         .attr("opacity", function(d, j) {
-          return j != i ? 0.6 : 1;
+          return j != i ? 0.2 : 1;
         })
     })
-  //   .on("mousemove", function(d, i) {
-  //     mousex = d3.mouse(this);
-  //     mousex = mousex[0];
-  //     var invertedx = x.invert(mousex);
-  //     invertedx = invertedx.getMonth() + invertedx.getDate();
-  //     var selected = (d.values);
-  //     for (var k = 0; k < selected.length; k++) {
-  //       datearray[k] = selected[k].date
-  //       datearray[k] = datearray[k].getMonth() + datearray[k].getDate();
-  //     }
-  //
-  //     mousedate = datearray.indexOf(invertedx);
-  //     pro = d.values[mousedate].value;
-  //
-  //     d3.select(this)
-  //     .classed("hover", true)
-  //     .attr("stroke", strokecolor)
-  //     .attr("stroke-width", "0.5px"),
-  //     tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "visible");
-  //
-  //   })
-  //   .on("mouseout", function(d, i) {
-  //    svg.selectAll(".layer")
-  //     .transition()
-  //     .duration(250)
-  //     .attr("opacity", "1");
-  //     d3.select(this)
-  //     .classed("hover", false)
-  //     .attr("stroke-width", "0px"), tooltip.html( "<p>" + d.key + "<br>" + pro + "</p>" ).style("visibility", "hidden");
-  // })
-  //
-  // var vertical = d3.select(".chart")
-  //       .append("div")
-  //       .attr("class", "remove")
-  //       .style("position", "absolute")
-  //       .style("z-index", "19")
-  //       .style("width", "1px")
-  //       .style("height", "380px")
-  //       .style("top", "10px")
-  //       .style("bottom", "30px")
-  //       .style("left", "0px")
-  //       .style("background", "#fff");
-  //
-  // d3.select(".chart")
-  //     .on("mousemove", function(){
-  //        mousex = d3.mouse(this);
-  //        mousex = mousex[0] + 5;
-  //        vertical.style("left", mousex + "px" )})
-  //     .on("mouseover", function(){
-  //        mousex = d3.mouse(this);
-  //        mousex = mousex[0] + 5;
-  //        vertical.style("left", mousex + "px")});
+    .on("mousemove", function(d, i) {
+      var mouse = d3.mouse(this);
+      var mouseX = mouse[0];
+      var mouseY = mouse[1];
 
-  var histogramSvg = d3.selectAll(".histogram").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", (barHeight + 10) + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      var invertedx = x.invert(mouseX);
+      var year = Math.round(invertedx.getUTCFullYear() / 10) * 10;
+      var category = d.key
 
-  function draw_histogram(category, d) {
-    var data = [];
-
-    d.forEach(function(d) {
-      if (d.category === category) {
-        data.push({
-          salary: +d.salary,
-          count: +d.count_all,
-          job: d.occupation
-        })
-      }
-    });
-    var x = d3.scaleLinear()
-      .domain(d3.extent(data, function(d) {
-        return d.salary;
-      }))
-      .range([0, width]);
-    var y = d3.scaleLinear()
-      .range([barHeight, 0]);
-
-    var histogram = d3.histogram()
-      .value(function(d) {
-        return d.count;
-      })
-      .domain(x.domain())
-      .thresholds(20);
-
-
-    var bin = histogram(data);
-
-    bin.forEach(function(d) {
-      d.count = 0;
-    });
-    data.forEach(function(d) {
-      bin.forEach(function(e) {
-        if (e.x0 <= d.salary && d.salary <= e.x1) {
-          e.count += d.count;
+      var data;
+      d.forEach(function(e) {
+        if (Array.isArray(e)) {
+          e.data.values.forEach(function(i) {
+            var dataYear = new Date(+e.data.key);
+            dataYear = dataYear.getUTCFullYear();
+            if (year === dataYear && i.category === category) {
+              data = i;
+            }
+          })
         }
       })
-    });
 
-    y.domain(d3.extent(bin, function(d) { return d.count }));
+      d3.select(this)
+        .classed("hover", true)
 
-    var bar = histogramSvg.selectAll(".bar")
-        .data(bin, function(d) {  }).enter().append("g")
-        .attr("class", "bar")
-        .attr("transform", function(d) { return "translate(" + x(d.x0) + ", " + y(d.count) + ")"; })
-    bar.append("rect")
-        .attr("x", 1)
-        .attr("width", function(d) { return x(bin[0].x1) - x(bin[0].x0) - 1 } )
-        .attr("height", function(d) { return barHeight - y(d.count); });
-  }
+      var tooltip = d3.select("#tooltip")
+        .classed("hidden", false)
+        .style("left", (mouseX + margin.left + 5) + "px")
+        .style("top", (mouseY + margin.top + 10) + "px")
+      tooltip.select("#year").text(year);
+      tooltip.select("#category").text(data.category);
+      tooltip.select("#count").text(data.count_category_all.toLocaleString());
+      tooltip.select("#population").text((+data.year_total).toLocaleString())
+    })
+    .on("mouseout", function(d, i) {
+      svg.selectAll(".layer")
+        .transition()
+        .duration(250)
+        .attr("opacity", "1");
+      d3.select(this)
+        .classed("hover", false)
+
+      d3.select("#tooltip").classed("hidden", true);
+    })
+
+  // var histogramSvg = d3.selectAll(".histogram").append("svg")
+  //   .attr("width", width + margin.left + margin.right)
+  //   .attr("height", (barHeight + 10) + margin.top + margin.bottom)
+  //   .append("g")
+  //   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  //
+  // function draw_histogram(category, d) {
+  //   var data = [];
+  //
+  //   d.forEach(function(d) {
+  //     if (d.category === category) {
+  //       data.push({
+  //         salary: +d.salary,
+  //         count: +d.count_all,
+  //         job: d.occupation
+  //       })
+  //     }
+  //   });
+  //   var x = d3.scaleLinear()
+  //     .domain(d3.extent(data, function(d) {
+  //       return d.salary;
+  //     }))
+  //     .range([0, width]);
+  //   var y = d3.scaleLinear()
+  //     .range([barHeight, 0]);
+  //
+  //   var histogram = d3.histogram()
+  //     .value(function(d) {
+  //       return d.count;
+  //     })
+  //     .domain(x.domain())
+  //     .thresholds(20);
+
+
+  //   var bin = histogram(data);
+  //
+  //   bin.forEach(function(d) {
+  //     d.count = 0;
+  //   });
+  //   data.forEach(function(d) {
+  //     bin.forEach(function(e) {
+  //       if (e.x0 <= d.salary && d.salary <= e.x1) {
+  //         e.count += d.count;
+  //       }
+  //     })
+  //   });
+  //
+  //   y.domain(d3.extent(bin, function(d) {
+  //     return d.count
+  //   }));
+  //
+  //   var bar = histogramSvg.selectAll(".bar")
+  //     .data(bin, function(d) {}).enter().append("g")
+  //     .attr("class", "bar")
+  //     .attr("transform", function(d) {
+  //       return "translate(" + x(d.x0) + ", " + y(d.count) + ")";
+  //     })
+  //   bar.append("rect")
+  //     .attr("x", 1)
+  //     .attr("width", function(d) {
+  //       return x(bin[0].x1) - x(bin[0].x0) - 1
+  //     })
+  //     .attr("height", function(d) {
+  //       return barHeight - y(d.count);
+  //     });
+  // }
 });
